0f05e28879e96e9a5c4b0f14f59860e865d6c9b6
topheryun <46732742+topheryun@users.noreply.github.com>
2020-12-23 17:23:35 -0800

    added TypeConversions.java

---
+++ src/main/java/com/revature/PokePipeline/Driver.java
@@ -0,0 +1,12 @@
+package com.revature.PokePipeline;
+
+import com.revature.PokePipeline.model.Type;
+import com.revature.PokePipeline.utility.TypeConversions;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// in case we need to run anything as a java application
+	}
+
+}
