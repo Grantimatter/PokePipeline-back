bbee8cea1f1f8d97d87b15147336b766acc73487
topheryun <46732742+topheryun@users.noreply.github.com>
2020-12-23 18:55:38 -0800

    added master servlet

--- src/main/java/com/revature/PokePipeline/servlets/MasterServlet.java
+++ src/main/java/com/revature/PokePipeline/servlets/MasterServlet.java
@@ -1,5 +1,30 @@
 package com.revature.PokePipeline.servlets;
 
-public class MasterServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MasterServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		res.setContentType("application/json");
+		res.setStatus(404);
+
+		final String URI = req.getRequestURI().replace("/PokePipeline/", "");
+		
+		switch (URI) {
+		case "login":
+			break;
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
 
 }
