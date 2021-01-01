e2a3a478c765f16a17844b099b6244fcab9e34a0
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-01 16:29:31 -0600

    Hibernate tests now operational

--- src/main/java/com/revature/pokepipeline/utility/HibernateUtility.java
+++ src/main/java/com/revature/pokepipeline/utility/HibernateUtility.java
@@ -6,20 +6,18 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtility {
 	
-	private static SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	private static SessionFactory sf;
 	private static Session ses;
 	
 	public static Session getSession() {
 		if (ses == null) {
-			ses = sf.openSession();
 			Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
 			configuration.setProperty("hibernate.connection.username", System.getenv("postgresUsername"));
-			configuration.setProperty("hibernate.connection.password", System.getenv("awsPassword"));
+			configuration.setProperty("hibernate.connection.password", System.getenv("postgresPassword"));
 			sf = configuration.buildSessionFactory();
 			ses = sf.openSession();
 		}
-		Configuration cf = new Configuration();
-		cf.setProperty("hibernate.connection.password", System.getenv("awsPassword"));
+
 		return ses;
 	}
 	
